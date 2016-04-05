8ab88e71fefde51c5cc39c805b7287922ecfcd99
afcajamarcar <afcajamarcar@unal.edu.co>
2016-04-05 16:28:09 -0500

    Agregado archivo main.js el cal no se había incluido en el commit pasado.

---
+++ grails-app/assets/javascripts/main.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Crow on 04/04/2016.
+ */
+$(function() {
+    var app_id = '219219088445493';
+    var scopes = 'public_profile,email, user_friends';
+    var btn_login = "<a href='#' id='loginFB' class='buttons'>Iniciar Sesión con Facebook</a>";
+    var div_session = "<div id = 'facebook-session'>"+
+                        "<strong></strong>"+
+                        "<img>"+
+                        "<a href='#' id='logoutFB' class='buttons'>Cerrar sesion</a>"+
+                        "<\div>";
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : app_id,
+            cookie     : true,
+            status     : true,
+            xfbml      : true,
+            version    : 'v2.5'
+        });
+
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response, function(){
+
+            });
+        });
+    };
+
+    var statusChangeCallback = function(response, callback) {
+        console.log('statusChangeCallback');
+        console.log(response);
+
+        if (response.status === 'connected') {
+            getFacebookData();
+        } else {
+            callback(false);
+        }
+    }
+
+    var checkLoginState = function(callback) {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response, function(data){
+                callback(data);
+            });
+        });
+    }
+
+    var getFacebookData = function(){
+        FB.api('/me', function(response) {
+            $('#loginFB').after(div_session);
+            $('#loginFB').remove();
+            $('#facebook-session strong').text("Bienvenido: "+response.name);
+            $('#facebook-session img').attr('src','http://graph.facebook.com/'+response.id+'/picture?type=small');
+        })
+    }
+
+    var facebookLogin = function(){
+        checkLoginState(function(response) {
+            if (!response) {
+                FB.login(function(response) {
+                    if (response.status == 'connected')
+                        getFacebookData();
+                }, {scope: scopes})
+            }
+        })
+    }
+
+    var facebookLogout = function() {
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                FB.logout(function(response) {
+                    $('#facebook-session').before(btn_login);
+                    $('#facebook-session').remove();
+                })
+            }
+        });
+
+    }
+
+    $(document).on('click', '#loginFB', function(e){
+        e.preventDefault();
+        facebookLogin();
+    })
+
+    $(document).on('click', '#logoutFB', function(e){
+        e.preventDefault();
+        if (confirm("¿Está seguro?"))
+            facebookLogout();
+        else
+            return false;
+    })
+
+})
